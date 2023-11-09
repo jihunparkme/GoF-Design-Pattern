91c6787c2dcd804c49257efd806777c2d6d33561
Jihun Park <jihunpark.tech@gmail.com>
2023-11-09 23:00:12 +0900

    [Design Pattern] Abstract Factory Method Pattern = Before

---
+++ src/main/java/me/whiteship/designpatterns/_01_creational_patterns/_03_abstract_factory/practice/WhiteWheel.java
@@ -0,0 +1,6 @@
+package me.whiteship.designpatterns._01_creational_patterns._03_abstract_factory.practice;
+
+import me.whiteship.designpatterns._01_creational_patterns._03_abstract_factory._02_after.Wheel;
+
+public class WhiteWheel implements Wheel {
+}
