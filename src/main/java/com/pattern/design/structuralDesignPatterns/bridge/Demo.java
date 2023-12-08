df12df1c789ce4f60e94526cf7921bc56e6cbec8
Jihun Park <jihunpark.tech@gmail.com>
2023-12-08 20:47:04 +0900

    [Design Pattern] Bridge Pattern: Practice

--- src/main/java/com/pattern/design/structuralDesignPatterns/bridge/Demo.java
+++ src/main/java/com/pattern/design/structuralDesignPatterns/bridge/Demo.java
@@ -15,6 +15,7 @@ public class Demo {
     /**
      * 클라이언트는 추상화와 작업하는데만 관심
      * - 그러나 추상화 객체를 구현 객체들 중 하나와 연결하는 것도 클라이언트의 역할
+     * - 원하는 유형의 리모컨을 특정 장치 객체와 연결
      */
     public static void testDevice(Device device) {
         System.out.println("Tests with basic remote.");
